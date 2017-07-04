654efbd8b0c9a7d54c91602883d66033fdcc9920
BarneyGordon <barnaby.gordon@hummingbirdtechgroup.com>
2017-07-04 17:29:21 +0100

    rastermask function

--- tools/gis.py
+++ tools/gis.py
@@ -4,6 +4,9 @@ from shapely.geometry import Polygon
 from shapely.ops import transform as shapely_transform
 from geojson import Feature
 from functools import partial
+import numpy as np
+from PIL import Image as PILImage
+from PIL import ImageDraw
 
 
 def world_to_pixel(x: float, y: float, geotransform: Geotransform) -> (int, int):
@@ -40,3 +43,20 @@ def transform_polygon(polygon: Polygon, in_epsg: int, out_epsg: int) -> Polygon:
 
 def wkt_to_geojson(polygon: Polygon, properties: dict=dict) -> Feature:
     return Feature(geometry=polygon, properties=properties)
+
+
+def clip_image(image: np.ndarray, polygon: Polygon) -> np.ndarray:
+    coordinates = np.array([(point[0], point[1]) for point in polygon.exterior.coords])
+
+    mask_image = PILImage.new("L", (image.shape[1], image.shape[0]), 1)
+    ImageDraw.Draw(mask_image).polygon(coordinates, 0)
+
+    mask = np.array(mask_image)
+    x_min, y_min = int(min(coordinates[:, 0])), int(min(coordinates[:, 1]))
+    x_max, y_max = int(max(coordinates[:, 0])), int(max(coordinates[:, 1]))
+
+    image_clip = image[y_min:y_max, x_min:x_max]
+    mask_clip = mask[y_min:y_max, x_min: x_max]
+    image_clip[mask_clip != 0] = np.nan
+
+    return image_clip
