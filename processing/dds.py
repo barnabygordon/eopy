0d21145370ef816cadf964095e633e11de661878
BarneyGordon <barnaby.gordon@hummingbirdtechgroup.com>
2017-07-04 16:59:04 +0100

    dds class

---
+++ processing/dds.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+class DDS:
+    """ Direct Decorrelation Stretch """
+    @staticmethod
+    def calculate(image: np.ndarray, k: float=0.6) -> np.ndarray:
+        """ Saturates an image according to the k factor
+        :param image: A 3D image with 3 bands and shape (columns, rows, bands)
+        :param k: The factor to which the image should be saturated
+        :return: A saturated image
+        """
+        r, g, b = image[:, :, 0], image[:, :, 1], image[:, :, 2]
+
+        x = np.amin(image, axis=0)
+
+        rk = r - (k*x)
+        gk = g - (k*x)
+        bk = b - (k*x)
+
+        return np.dstack((rk, gk, bk))
