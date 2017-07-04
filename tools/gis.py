720845a8aabf6926e378f8ba7df8ebcaa178d9a0
BarneyGordon <barnaby.gordon@hummingbirdtechgroup.com>
2017-07-04 17:11:45 +0100

    pixel to world function

--- tools/gis.py
+++ tools/gis.py
@@ -6,3 +6,10 @@ def world_to_pixel(x: float, y: float, geotransform: Geotransform) -> (int, int)
     y = int((y - geotransform.upper_left_y) / geotransform.pixel_width)
 
     return x, -y
+
+
+def pixel_to_world(x: int, y: int, geotransform: Geotransform) -> (float, float):
+    x2 = (y * geotransform.pixel_width) + geotransform.upper_left_y
+    y2 = (x * geotransform.pixel_width) + geotransform.upper_left_x
+
+    return x2, y2
