4127859cb47be6e8220b4066127c5f4d0231623b
Barney <barney@tbotix.com>
2017-07-08 10:34:11 +0100

    updated to work with new url

--- image/downloader.py
+++ image/downloader.py
@@ -1,25 +1,18 @@
 from osgeo import gdal
 
-from image import LandsatSceneID
+from image import LandsatScene
 from image import Image
 
-LANDSAT_8_BANDS = [
-    'coastal', 'blue', 'green', 'red', 'nir', 'swir_1',
-    'swir_2', 'pan', 'cirrus', 'tirs_1', 'tirs_2']
 LANDSAT_8_LOOKUP_BAND = {
-    'coastal': 'B1', 'blue': 'B2', 'green': 'B3', 'red': 'B4',
-    'nir': 'B5', 'swir_1': 'B6', 'swir_2': 'B7', 'pan': 'B8',
-    'cirrus': 'B9', 'tirs_1': 'B10', 'tirs_2': 'B11'}
+    'coastal': 1, 'blue': 2, 'green': 3, 'red': 4,
+    'nir': 5, 'swir_1': 6, 'swir_2': 7, 'pan': 8,
+    'cirrus': 9, 'tirs_1': 10, 'tirs_2': 11, 'BQA': 12}
 
 
 class Downloader:
-    def download_landsat8_band(self, scene_id: LandsatSceneID, band):
-        url = 'https://landsat-pds.s3.amazonaws.com/L8/{path}/{row}/{scene_id}/{scene_id}_{band}.TIF'.format(
-            path=scene_id.path,
-            row=scene_id.row,
-            band=LANDSAT_8_LOOKUP_BAND[band],
-            scene_id=scene_id.string)
-
+    @staticmethod
+    def get_landsat8_band(scene: LandsatScene, band) -> Image:
+        url = scene.download_links[LANDSAT_8_LOOKUP_BAND[band]]
         image_dataset = gdal.Open('/vsicurl/{}'.format(url))
 
         return Image(image_dataset)
