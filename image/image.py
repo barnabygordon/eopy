d953c515f505d90cd6198d745f5abd68b70c4c75
Barney <barney@tbotix.com>
2017-07-08 20:50:19 +0100

    load image using image class

--- image/image.py
+++ image/image.py
@@ -1,13 +1,18 @@
 import numpy as np
-from image.geotransform import Geotransform
+from osgeo import gdal
 
-BAND_LOOKUP = {'blue': 1, 'green': 2, 'red': 3, 'red_edge': 4, 'nir': 5}
+from image.geotransform import Geotransform
 
 
 class Image:
     def __init__(self, image_dataset):
         self.image_dataset = image_dataset
         self.geotransform = Geotransform(self.image_dataset)
+
+    @classmethod
+    def load(cls, filepath):
+        image_dataset = gdal.Open(filepath)
+        return Image(image_dataset)
 
     @property
     def width(self) -> int:
@@ -34,16 +39,6 @@ class Image:
     @property
     def pixels(self) -> np.ndarray:
         return self.image_dataset.ReadAsArray().transpose(1, 2, 0)
-
-    @property
-    def ndvi(self) -> np.ndarray:
-        nir = self.get_band('nir')
-        red = self.get_band('red')
-        return (nir - red) / (nir + red)
-
-    def get_band(self, band: str) -> np.ndarray:
-        band_dataset = self.image_dataset.GetRasterBand(BAND_LOOKUP[band])
-        return band_dataset.ReadAsArray()
 
     def get_window(self, x: int, y: int, width: int) -> np.ndarray:
         return self.image_dataset.ReadAsArray(x, y, width, width)
