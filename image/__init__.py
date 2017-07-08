4e90a23dff8fd2ff5858809d994f62712ebd1fc5
Barney <barney@tbotix.com>
2017-07-08 19:24:51 +0100

    seperate out classes

--- image/__init__.py
+++ image/__init__.py
@@ -1,5 +1,3 @@
 from .image import Image
 from .searcher import Searcher
-from .searcher import LandsatScene
 from .downloader import Downloader
-from .geotransform import Geotransform
--- image/searcher.py
+++ image/searcher.py
@@ -6,29 +6,8 @@ from shapely.geometry import Polygon
 from xml.etree import ElementTree
 
 from tools import gis
+from image.scene import LandsatScene, SentinelScene
 import config
-
-
-class LandsatScene:
-    """ Landsat-8 scene metadata """
-    def __init__(self, scene_id: str, date: str, clouds: float, bounds: Polygon, download_links: [str]):
-        self.string = scene_id
-        self.path = self.string[3:6]
-        self.row = self.string[6:9]
-        self.date = date
-        self.bounds = bounds
-        self.clouds = int(clouds)
-        self.download_links = download_links
-
-
-class SentinelScene:
-    """ Sentinel-2 scene metadata """
-    def __init__(self, scene_id: str, date: str, clouds: float, bounds: Polygon, image_url: str):
-        self.scene_id = scene_id
-        self.date = date
-        self.clouds = clouds
-        self.bounds = bounds
-        self.image_url = image_url
 
 
 class Searcher:
