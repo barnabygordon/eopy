334a00dc2f0cfb0d5a8229485e6fb5f943fe036b
Barney <barney@tbotix.com>
2017-07-08 19:45:36 +0100

    def for saving images

--- image/downloader.py
+++ image/downloader.py
@@ -1,4 +1,5 @@
 from osgeo import gdal
+import numpy as np
 
 from image.scene import LandsatScene, SentinelScene
 from image import Image
@@ -7,6 +8,7 @@ LANDSAT_8_LOOKUP_BAND = {
     'coastal': 1, 'blue': 2, 'green': 3, 'red': 4,
     'nir': 5, 'swir_1': 6, 'swir_2': 7, 'pan': 8,
     'cirrus': 9, 'tirs_1': 10, 'tirs_2': 11, 'BQA': 12}
+GTIFF_DRIVER = 'GTiff'
 
 
 class Downloader:
@@ -30,3 +32,30 @@ class Downloader:
     @staticmethod
     def get_sentinel2_band(scene: SentinelScene, band: str) -> Image:
         raise NotImplementedError("Sorry! Work in progress!")
+
+    def save_image(self,
+                   image: np.ndarray,
+                   image_dataset: gdal.Dataset,
+                   filepath: str,
+                   data_type: int = gdal.GDT_Float32) -> None:
+
+        width = image.shape[0]
+        height = image.shape[1]
+
+        if image.ndims > 2:
+            number_of_bands = image.shape[2]
+        else:
+            number_of_bands = 1
+
+        out_image = gdal.GetDriverByName(GTIFF_DRIVER)
+        out_image.Create(filepath, height, width, number_of_bands, data_type)
+        out_image.SetGeoTransform(image_dataset.GetGeoTransform())
+        out_image.SetProjection(image_dataset.GetProjection())
+
+        if number_of_bands > 1:
+            for band in number_of_bands:
+                out_image.GetRasterBand(band+1).WriteArray(image[:, :, band])
+        else:
+            out_image.GetRasterBand(1).WriteArray(image)
+
+        out_image.FlushCache()
