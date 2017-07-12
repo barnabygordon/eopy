2487a202289ea2f71939dd3192db13e0de746a61
BarneyGordon <barnaby.gordon@hummingbirdtechgroup.com>
2017-07-12 16:55:49 +0100

    docs

--- image/image.py
+++ image/image.py
@@ -6,6 +6,7 @@ from image.geotransform import Geotransform
 
 
 class Image:
+    """ A generic class for dealing with GDAL images """
     def __init__(self, image_dataset: gdal.Dataset):
         self.image_dataset = image_dataset
         self.geotransform = Geotransform(self.image_dataset)
@@ -25,6 +26,7 @@ class Image:
 
     @property
     def bounds(self) -> ([float], [float]):
+        """ The boundary of the image as a polygon """
         x_max = self.width * self.geotransform.pixel_width
         y_min = self.height * self.geotransform.pixel_width
         x = [self.geotransform.upper_left_x, x_max, x_max,
@@ -36,14 +38,17 @@ class Image:
 
     @property
     def band_count(self) -> int:
+        """ Number of bands in the image """
         return self.image_dataset.RasterCount
 
     @property
     def pixels(self) -> np.ndarray:
+        """ The pixels data as a ndarray """
         return self.image_dataset.ReadAsArray().transpose(1, 2, 0)
 
     @property
     def projection(self) -> str:
+        """ The projection of the image as a WKT string """
         return self.image_dataset.GetProjection()
 
     @property
@@ -52,4 +57,5 @@ class Image:
         return spatial_reference.GetAttrValue("AUTHORITY", 1)
 
     def get_window(self, x: int, y: int, width: int) -> np.ndarray:
+        """ A slice of the image across all bands """
         return self.image_dataset.ReadAsArray(x, y, width, width)
