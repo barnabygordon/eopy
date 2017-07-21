66abb47c277d2ef0cd28f8c03fb359dd15b7dc5e
BarneyGordon <barnaby.gordon@hummingbirdtechgroup.com>
2017-07-21 16:48:59 +0100

    docs

--- image/calibration.py
+++ image/calibration.py
@@ -6,11 +6,17 @@ from image import Landsat8
 
 
 class Calibration:
+    """ Functionality to calibrate satellite data """
     def __init__(self, metadata_url: str):
         self.landsat8 = Landsat8()
         self.metadata_file = urlopen(metadata_url)
 
-    def calibrate_landsat(self, image: np.ndarray, band_list: [str]):
+    def calibrate_landsat(self, image: np.ndarray, band_list: [str]) -> np.ndarray:
+        """ Calibrate a stack of Landsat bands
+        :param image: Stack of Landsat bands with shape (columns, rows, bands)
+        :param band_list: List of band names
+        :return: Stack of calibrated Landsat bands
+        """
         calibrated_image = np.zeros(image.shape)
         for i, band_name in enumerate(band_list):
             band_array = image[:, :, i]
@@ -20,26 +26,31 @@ class Calibration:
 
         return calibrated_image
 
-    def calibrate_landsat_band(self, band_array, band_number):
-        gain = self.get_landsat_metadata_value(self.metadata_file, 'REFLECTANCE_MULT_BAND_{}'.format(band_number))
-        bias = self.get_landsat_metadata_value(self.metadata_file, 'REFLECTANCE_ADD_BAND_{}'.format(band_number))
-        sun_elevation_degrees = self.get_landsat_metadata_value(self.metadata_file, 'SUN_ELEVATION')
+    def calibrate_landsat_band(self, band_array: np.ndarray, band_number: int) -> np.ndarray:
+        """ Calibrate a single Landsat band
+        :param band_array: 2D numpy array
+        :param band_number: Number of the band
+        :return: 2D calibrated numpy array
+        """
+        gain = self.get_landsat_metadata_value('REFLECTANCE_MULT_BAND_{}'.format(band_number))
+        bias = self.get_landsat_metadata_value('REFLECTANCE_ADD_BAND_{}'.format(band_number))
+        sun_elevation_degrees = self.get_landsat_metadata_value('SUN_ELEVATION')
         sun_elevation_radians = math.radians(sun_elevation_degrees)
 
         return self.calculate_landsat_toa_reflectance(band_array, gain, bias, sun_elevation_radians)
 
-    def get_landsat_metadata_value(self, metadata_file, parameter):
+    def get_landsat_metadata_value(self, parameter):
         """ Extract value from metadata txt file
-        :param metadata_file: Iterable from urllib.request.urlopen
         :param parameter: The desired parameter
         :return: value
         """
-        for line in metadata_file:
+        for line in self.metadata_file:
             data = line.split(' = ')
             if(data[0]).strip() == parameter:
                 return (data[1]).strip()
 
-    def calculate_landsat_toa_reflectance(self, dn: np.ndarray, gain: float, bias: float, sun_elevation: float):
+    @staticmethod
+    def calculate_landsat_toa_reflectance(dn: np.ndarray, gain: float, bias: float, sun_elevation: float):
         """ Calculate Top of Atmosphere reflectance taking into account solar geometry
         :param dn: Digital Number
         :param gain: The band specific multiplicative rescaling factor
