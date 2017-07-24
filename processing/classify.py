d5add1b16a5cf76a3c59a7f32abb38813f63c21f
BarneyGordon <barnaby.gordon@hummingbirdtechgroup.com>
2017-07-24 18:26:43 +0100

    rearrange function parameters

--- processing/classify.py
+++ processing/classify.py
@@ -9,44 +9,44 @@ from sklearn.ensemble import RandomForestClassifier
 
 class Classifier:
     """ A Random Forest-based pixel-by-pixel classifer that requires a shapefile delineating classes"""
-    def __init__(self, image: Image, class_data_filepath: str, class_name: str='label', n_estimators: int=100):
+    def __init__(self, image: Image, classifier=RandomForestClassifier, n_estimators=100):
         """
         :param image: An image object with n-number of bands
-        :param class_data_filepath: Path to the shapefile/geojson
-        :param class_name: name used for class attribution
         :param n_estimators: number of estimators to be used in the Random Forest
         """
         self.image = image
-        self.class_data = self._open_class_file(class_data_filepath)
-        self.classifier = RandomForestClassifier(n_estimators=n_estimators)
-        self.class_name = class_name
+        self.classifier = classifier(n_estimators)
         self.trained = False
 
     def _open_class_file(self, filepath: str) -> gpd.GeoDataFrame:
+        """ Open a shapefile and return a geopandas dataframe """
         class_data = gpd.read_file(filepath)
 
         class_data.geometry = class_data.geometry.apply(
-            lambda x: gis.polygon_to_pixel(x,
-                                           self.image.geotransform,
-                                           self.image.height))
+            lambda x: gis.polygon_to_pixel(x, self.image.geotransform))
 
         return class_data
 
-    def train(self) -> None:
-        """ Train the model using the image data and labels """
+    def train(self, class_data_filepath: str, class_name: str='label') -> None:
+        """ Train a model using a shapefile as labelled class data
+        :param class_data_filepath: Path to the shapefile containing class polygons
+        :param class_name: Name of the attribute column
+        """
+        class_data = self._open_class_file(class_data_filepath)
+
         classes, class_values = [], []
         features = []
-        for i, row in self.class_data.iterrows():
+        for i, row in class_data.iterrows():
             clip = gis.clip_image(self.image.pixels, row.geometry)
             flat_clip = clip.reshape(clip.shape[0] * clip.shape[1], clip.shape[2])
             clean_features = [features for features in flat_clip if not np.isnan(np.sum(features))]
 
             features.extend(clean_features)
 
-            if row[self.class_name] not in classes:
-                classes.append(row[self.class_name])
+            if row[class_name] not in classes:
+                classes.append(row[class_name])
 
-            class_value = classes.index(row[self.class_name])
+            class_value = classes.index(row[class_name])
             class_values.extend([class_value] * len(clean_features))
 
         self.classifier.fit(features, class_values)
@@ -65,10 +65,10 @@ class Classifier:
         # TODO: return class lookup table
         return predictions.reshape((test_image.shape[0], test_image.shape[1]))
 
-    def show(self, figsize: (int, int)=(15, 10)):
+    def show(self, class_file_path: str, figsize: (int, int)=(15, 10)):
         """ Plot the image data with the labels overlain """
         f, ax = plt.subplots(figsize=figsize)
 
         ax.imshow(self.image.pixels[:, :, 0])
-        self.class_data.plot(ax=ax, column='label', legend=True)
+        self._open_class_file(class_file_path).plot(ax=ax, column='label', legend=True)
         plt.show()
