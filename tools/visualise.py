6a908b1767fab5ae67e06965561c577d5f763f07
BarneyGordon <barnaby.gordon@hummingbirdtechgroup.com>
2017-07-25 16:11:30 +0100

    documentation

--- tools/visualise.py
+++ tools/visualise.py
@@ -5,8 +5,13 @@ import folium
 
 
 class Visualise:
+    """ Class for visualising geospatial data"""
     @staticmethod
     def image_bounds(image: Image):
+        """ Display image boundary on a slippy Leaflet map
+        :param image: An image object
+        :return: A Folium map instance (will display automatically in Jupyter)
+        """
         unprojected_bounds = gis.transform_polygon(image.bounds,
                                                    in_epsg=image.epsg,
                                                    out_epsg=gis.WGS84_EPSG)
