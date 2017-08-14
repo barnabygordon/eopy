05757759541dfda48161bfcc4caa4817f432c0f7
Barney <barney@tbotix.com>
2017-08-14 21:26:09 +0100

    vectorise superpixels

--- classification/superpixels.py
+++ classification/superpixels.py
@@ -1,4 +1,7 @@
 from skimage.segmentation import slic
+from shapely.geometry import Polygon
+import matplotlib.pyplot as plt
+import geopandas as gpd
 
 from image import Image
 
@@ -24,4 +27,34 @@ class Superpixels:
         segments = slic(self.image, n_segments=self.n_segments, compactness=self.compactness, sigma=self.sigma,
                         multichannel=multichannel, enforce_connectivity=self.enforce_connectivity)
 
-        return segments
+        superpixel_list = []
+        for i in range(segments.max()):
+            segment = segments == i
+            superpixel_list.append(self._vectorise_superpixel(segment))
+
+        superpixels = gpd.GeoDataFrame(geometry=superpixel_list)
+
+        return superpixels
+
+    @staticmethod
+    def _vectorise_superpixel(segment):
+        contour_collection = plt.contour(segment, levels=[0])
+        plt.clf()
+
+        contour_polygons = []
+        for i, contour in enumerate(contour_collection.collections):
+            for path in contour.get_paths():
+                path.should_simplify = False
+                polygon = path.to_polygons()
+
+                holes, exterior = [], []
+                if len(polygon) > 0 and len(polygon[0]) > 5:
+                    exterior = polygon[0]
+                    if len(polygon) > 1:
+                        holes = [h for h in polygon[1:] if len(h) > 5]
+
+                if len(exterior) > 5:
+                    polygon = Polygon(exterior, holes)
+                    contour_polygons.append(polygon)
+
+        return contour_polygons[0]
