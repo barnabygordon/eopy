6347818e3e0695e417d6c66582d63c2baeecc5a7
BarneyGordon <barnaby.gordon@hummingbirdtechgroup.com>
2017-09-05 16:42:59 +0100

    image plot saving

--- tools/visualise.py
+++ tools/visualise.py
@@ -1,5 +1,7 @@
 import folium
 import geojson
+import numpy as np
+import matplotlib.pyplot as plt
 
 from cloud.scene import SatelliteScene
 from image import Image
@@ -15,8 +17,8 @@ class Visualise:
         :return: A Folium map instance (will display automatically in Jupyter)
         """
         unprojected_bounds = [gis.transform_polygon(image.bounds,
-                                                   in_epsg=image.epsg,
-                                                   out_epsg=gis.WGS84_EPSG) for image in image_list]
+                                                    in_epsg=image.epsg,
+                                                    out_epsg=gis.WGS84_EPSG) for image in image_list]
         geojson_bounds = [geojson.Feature(geometry=bounds) for bounds in unprojected_bounds]
         center = unprojected_bounds[0].centroid.xy
         map_window = folium.Map(location=[center[1][0], center[0][0]])
@@ -41,3 +43,16 @@ class Visualise:
                 }).add_to(map_window)
 
         return map_window
+
+    @staticmethod
+    def save_pyplot(image: np.ndarray, filepath: str, width_in_inches: float, height_in_inches: float, dpi: int):
+        """ Plot an image and save it without whitespace """
+        f = plt.figure(frameon=False)
+        f.set_size_inches(width_in_inches, height_in_inches)
+
+        ax = plt.Axes(f, [0., 0., 1., 1.])
+        ax.set_axis_off()
+        f.add_axes(ax)
+
+        ax.imshow(image, aspect='normal')
+        f.savefig(filepath, dpi=dpi)
