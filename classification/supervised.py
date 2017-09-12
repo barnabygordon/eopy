4eb4d5200d65a4d49aa36280d07a970d305f1292
BarneyGordon <barnaby.gordon@hummingbirdtechgroup.com>
2017-09-12 09:21:33 +0100

    docs

--- classification/supervised.py
+++ classification/supervised.py
@@ -4,56 +4,48 @@ from tools import gis
 import numpy as np
 import geopandas as gpd
 import matplotlib.pyplot as plt
+from shapely.geometry import Polygon
 from sklearn.ensemble import RandomForestClassifier
 
 
 class Supervised:
-    def __init__(self, image, vector_filepath, label_name='class', model=RandomForestClassifier, estimators=100):
+    """ A pixel-based classifier that uses the RandomForest model
+        :param image: Image
+        :param vector_filepath: Path to shapefile
+        :param label_name: Column name of labels
+        :param model: Type of model to be trained
+        :param estimators: Number of estimators
+    """
+    def __init__(self,
+                 image: Image,
+                 vector_filepath: str,
+                 label_name: str='class',
+                 model=RandomForestClassifier,
+                 estimators: int=100):
         self.image = image
         self.vectors = self._gather_data(vector_filepath)
         self.label_name = label_name
         self.model = model(estimators)
         self.trained = False
 
-    def _gather_data(self, vector_filepath):
+    def _gather_data(self, vector_filepath: str) -> gpd.GeoDataFrame:
+        """ Extract all features for class vectors """
         gdf = gpd.read_file(vector_filepath)
         gdf['pixel_polygon'] = gdf.geometry.apply(lambda x: gis.polygon_to_pixel(x, self.image.geotransform))
         gdf['features'] = gdf.pixel_polygon.apply(lambda x: self._extract_features(x))
 
         return gdf
 
-    def _extract_features(self, polygon):
+    def _extract_features(self, polygon: Polygon) -> np.ndarray:
+        """ Extract all features and remove nan values """
         clipped_image = self.image.clip_with(polygon)
         features = clipped_image.pixels.reshape((clipped_image.width * clipped_image.height, clipped_image.band_count))
         clean_features = np.array([feature for feature in features if not np.isnan(feature.min())])
 
         return clean_features
 
-    def train_model(self):
-        labels, features = [], []
-
-        for c in self.vectors[self.label_name].unique():
-            all_features = np.vstack(self.vectors.loc[self.vectors[self.label_name] == c].features.tolist())
-
-            features.extend(all_features)
-            labels.extend([self.vectors[self.label_name].unique().tolist().index(c)] * len(all_features))
-
-        self.model.fit(features, labels)
-        self.trained = True
-
-    def test_model(self, image):
-        if not self.trained:
-            raise UserWarning("Model needs to be trained before it can be tested.")
-        features = image.pixels.reshape(image.width * image.height, image.band_count)
-        features[np.isnan(features)] = 0
-
-        results_list = self.model.predict(features)
-        results_image = results_list.reshape(image.width, image.height)
-        results_image[image[0].pixels == np.nan] = 0.
-
-        return results_image
-
     def plot_features(self):
+        """ Plot the averages of all class features and their variance """
         plt.figure(figsize=(15, 10))
 
         for c in self.vectors[self.label_name].unique():
@@ -67,3 +59,29 @@ class Supervised:
 
         plt.legend()
         plt.show()
+
+    def train_model(self) -> None:
+        """ Train the model """
+        labels, features = [], []
+
+        for c in self.vectors[self.label_name].unique():
+            all_features = np.vstack(self.vectors.loc[self.vectors[self.label_name] == c].features.tolist())
+
+            features.extend(all_features)
+            labels.extend([self.vectors[self.label_name].unique().tolist().index(c)] * len(all_features))
+
+        self.model.fit(features, labels)
+        self.trained = True
+
+    def test_model(self, image: Image) -> Image:
+        """ Run the trained model on an image """
+        if not self.trained:
+            raise UserWarning("Model needs to be trained before it can be tested.")
+        features = image.pixels.reshape(image.width * image.height, image.band_count)
+        features[np.isnan(features)] = 0
+
+        results_list = self.model.predict(features)
+        results_image = results_list.reshape(image.width, image.height)
+        results_image[image[0].pixels == np.nan] = 0.
+
+        return Image(results_image, self.image.geotransform, self.image.projection)
