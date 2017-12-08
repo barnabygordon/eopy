ab93261d5ed3e2f4b5bd1effb46ffa7b1c8b7eb3
BarneyGordon <barney@hummingbirdtech.com>
2017-12-08 17:59:29 +0000

    use new geotransform

--- remotesensing/image/geotransform.py
+++ remotesensing/image/geotransform.py
@@ -2,17 +2,46 @@
 
 class Geotransform:
     """ A class for easy access to the GDAL geotransform tuple """
-    def __init__(self, geotransform_tuple):
+    def __init__(self, upper_left_x, upper_left_y, pixel_width, pixel_height, rotation_x, rotation_y):
         """
-        :type geotransform_tuple: tuple(float)
+        :type upper_left_x: float
+        :type upper_left_y: float
+        :type pixel_width: float
+        :type pixel_height: float
+        :type rotation_x: float
+        :type rotation_y: float
         """
-        self.tuple = geotransform_tuple
-        self.upper_left_x = self.tuple[0]
-        self.upper_left_y = self.tuple[3]
-        self.pixel_width = self.tuple[1]
-        self.pixel_height = self.tuple[5]
-        self.rotation_x = self.tuple[2]
-        self.rotation_y = self.tuple[4]
+
+        self._upper_left_x = upper_left_x
+        self._upper_left_y = upper_left_y
+        self._pixel_width = pixel_width
+        self._pixel_height = pixel_height
+        self._rotation_x = rotation_x
+        self._rotation_y = rotation_y
+
+    @property
+    def upper_left_x(self):
+        return self._upper_left_x
+
+    @property
+    def upper_left_y(self):
+        return self._upper_left_y
+
+    @property
+    def pixel_width(self):
+        return self._pixel_width
+
+    @property
+    def pixel_height(self):
+        return self._pixel_height
+
+    @property
+    def rotation_x(self):
+        return self._rotation_x
+
+    @property
+    def rotation_y(self):
+        return self._rotation_y
 
     def __repr__(self):
         return "(ulx, uly): ({}, {}) | xdist: {} ".format(self.upper_left_x, self.upper_left_y, self.pixel_width)
--- remotesensing/image/loader.py
+++ remotesensing/image/loader.py
@@ -26,7 +26,7 @@ class Loader:
         :type extent: shapely.geometry.Polygon
         :rtype: remotesensing.image.image.Image
         """
-        geotransform = Geotransform(image_dataset.GetGeoTransform())
+        geotransform = self._load_geotransform(image_dataset)
         projection = image_dataset.GetProjection()
         epsg = osr.SpatialReference(wkt=projection).GetAttrValue("AUTHORITY", 1)
         pixel_polygon = gis.polygon_to_pixel(gis.transform_polygon(extent, in_epsg=4326, out_epsg=epsg), geotransform)
@@ -48,7 +48,7 @@ class Loader:
         :type band_labels: dict
         :rtype: remotesensing.image.image.Image
         """
-        geotransform = Geotransform(image_dataset.GetGeoTransform())
+        geotransform = self._load_geotransform(image_dataset)
         projection = image_dataset.GetProjection()
         pixels = image_dataset.ReadAsArray()
 
@@ -56,3 +56,20 @@ class Loader:
             pixels = pixels.transpose(1, 2, 0)
 
         return Image(pixels, geotransform, projection, band_labels=band_labels)
+
+    def _load_geotransform(self, image_dataset):
+        """
+        :type image_dataset: osgeo.gdal.Dataset
+        :rtype: remotesensing.image.image.Geotransform
+        """
+
+        geotransform_values = image_dataset.GetGeoTransform()
+
+        return Geotransform(
+            upper_left_x=geotransform_values[0],
+            upper_left_y=geotransform_values[3],
+            pixel_width=geotransform_values[1],
+            pixel_height=geotransform_values[5],
+            rotation_x=geotransform_values[2],
+            rotation_y=geotransform_values[4]
+        )
