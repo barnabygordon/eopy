dda41c9462c9a783209f9b46c75cb93dcdb9d256
Barney Gordon <barney@9fin.com>
2020-07-11 10:31:27 +0100

    Move mosaic into processing

--- remotesensing/image/__init__.py
+++ remotesensing/image/__init__.py
@@ -1,4 +1,3 @@
 from remotesensing.image.geotransform import Geotransform
 from remotesensing.image.image import Image
 from remotesensing.image.loader import Loader
-from remotesensing.image.mosaic import Mosaic
