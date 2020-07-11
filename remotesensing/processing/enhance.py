c31ca326cee5fb7ed9e5bd1ee92cadc6f6461883
Barney Gordon <barney@9fin.com>
2020-07-11 10:31:54 +0100

    Restructure processing functions

--- remotesensing/processing/enhance.py
+++ remotesensing/processing/enhance.py
@@ -1,30 +1,35 @@
 import numpy as np
+from typing import Tuple
 
 from remotesensing.image import Image
 
 
-def bcet(image: Image, L: float = 0., H: float = 1., E: float = 0.5, clip: float = 0., window: slice = None) -> Image:
+def linear(image: Image, contrast: float, brightness: float) -> Image:
+
+    return image.apply(lambda x: contrast * x + brightness)
+
+
+def bcet(image: Image, value_range: Tuple[int, int] = (0, 1), clip: float = 0., window: slice = None) -> Image:
+    ''' BCET (Balanced Contrast Enhancement Technique)
+    G.J. Liu (1990) Balance contrast enhancement technique and its application in image colour composition
+    '''
+
+    L, H = value_range
+    E = H / 2
 
     bands = []
     for band in image:
 
-        if window:
-            pixels = band.pixels[window]
-        else:
-            pixels = band.pixels
+        pixels = band.pixels[window] if window else band.pixels
 
-        l0 = np.ma.min(pixels)
-        h0 = np.ma.max(pixels)
-        e = np.ma.mean(pixels)
+        l0 = np.nanmin(pixels)
+        h0 = np.nanmax(pixels)
+        e = np.nanmean(pixels)
 
         l = l0 + (clip * (h0-l0))
         h = h0 - (clip * (h0-l0))
 
-        L = L
-        H = H
-        E = E
-
-        s = np.ma.mean(pixels**2)
+        s = np.nanmean(pixels**2)
 
         b = (h**2 * (E - L) - s * (H - L) + l**2 * (H - E)) / (2 * (h * (E - L) - e * (H - L) + l * (H - E)))
         a = (H - L) / ((h - l) * (h + l - 2 * b))
@@ -32,9 +37,19 @@ def bcet(image: Image, L: float = 0., H: float = 1., E: float = 0.5, clip: float
 
         bands.append(band.apply(lambda x: a * (x - b)**2 + c))
 
-    bcet_image = image.stack(bands)
+    bcet_image = Image.stack(bands)
 
-    bcet_image.pixels[bcet_image.pixels > H] = H
-    bcet_image.pixels[bcet_image.pixels < L] = L
+    bcet_image.pixels[~np.isnan(bcet_image.pixels) > H] = H
+    bcet_image.pixels[~np.isnan(bcet_image.pixels) < L] = L
 
     return bcet_image
+
+
+def dds(image: Image, k: float = 0.6) -> Image:
+    ''' DDS (Direct Decorrelation Stretch)
+    J.G. Liu & McM Moore (1995) Direct decorrelation stretch technique for RGB colour composition
+    '''
+
+    x = np.nanmin(image.pixels, axis=2)
+
+    return Image.stack([band.apply(lambda i: i - (k * x)) for band in image])
--- remotesensing/processing/pansharpening.py
+++ remotesensing/processing/pansharpening.py
@@ -1,5 +1,4 @@
 import numpy as np
-from tqdm import tqdm
 from skimage.morphology import disk
 from skimage.filters import rank
 from skimage.transform import resize
@@ -7,26 +6,24 @@ from skimage.transform import resize
 from remotesensing.image import Image
 
 
-class SFIM:
-    """ (Smoothing Filter-Based Intensity Modulation)
-    A class for fusing together images for pansharpening
+def sfim(msi: Image, pan: Image, filter_size: int = None) -> Image:
+    """ SFIM (Smoothing Filter-Based Intensity Modulation)
+    J.G. Liu (1999) Smoothing Filter-based Intensity Modulation: a spectral preserve
+    image fusion technique for improving spatial details
     """
 
-    @classmethod
-    def calculate(cls, coarse: Image, fine: Image, filter_size: int = None) -> Image:
+    if not filter_size:
+        filter_size = pan.geotransform.pixel_width // msi.geotransform.pixel_width
 
-        if not filter_size:
-            filter_size = fine.geotransform.pixel_width // coarse.geotransform.pixel_width
+    smooth_pan = rank.mean(pan.pixels, disk(filter_size))
 
-        smooth_pan = rank.mean(fine.pixels, disk(filter_size))
+    output_shape = (pan.height, pan.width, msi.band_count)
+    msi_upsampled = pan.apply(lambda x: resize(msi.pixels, output_shape, preserve_range=True))
 
-        output_shape = (fine.height, fine.width, coarse.band_count)
-        msi_upsampled = fine.apply(lambda x: resize(coarse.pixels, output_shape, preserve_range=True))
+    bands = []
+    for band in msi_upsampled:
 
-        bands = []
-        for band in msi_upsampled:
+        numerator = band.pixels * pan.pixels
+        bands.append(band.apply(lambda x: np.divide(numerator, smooth_pan, out=np.zeros_like(numerator), where=smooth_pan != 0)))
 
-            numerator = band.pixels * fine.pixels
-            bands.append(band.apply(lambda x: np.divide(numerator, smooth_pan, out=np.zeros_like(numerator), where=smooth_pan != 0)))
-
-        return Image.stack(bands)
+    return Image.stack(bands)
