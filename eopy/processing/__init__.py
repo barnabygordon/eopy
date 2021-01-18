2420a790d092ed3300e3a2459bf60cff4860596f
Barney Gordon <barnaby_gordon@yahoo.com>
2021-01-18 22:04:01 +0000

    Remove PC

--- eopy/processing/__init__.py
+++ eopy/processing/__init__.py
@@ -1,3 +1,2 @@
 from .pca import ImagePCA
-from.registration import PCRegistrator, FeatureRegistrator
 from .mosaic import Mosaic
--- eopy/processing/registration/__init__.py
+++
@@ -1,2 +0,0 @@
-from eopy.processing.registration.phase_correlation import PCRegistrator
-from eopy.processing.registration.feature import FeatureRegistrator
